f5af8cc4f5babce8395ebe7e6a623f0083b607ba
Andrew McKnight <andrew@layer.com>
2017-01-31 12:45:00 -0800

    add custom view to capture a signature drawing

---
+++ Code/Views/ATLSignatureInputView.h
@@ -0,0 +1,15 @@
+//
+//  ATLSignatureInputView.h
+//  Pods
+//
+//  Created by Andrew Mcknight on 1/31/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ATLSignatureInputView : UIView
+
++ (CGSize)preferredSize;
+
+@end
